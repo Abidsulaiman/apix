f2ef038e93063682e8617c6fcbdfa73bc612c230
abidsulaiman <abidqubit@gmail.com>
2022-08-29 09:46:43 +0530

    analytics page updated

--- js/chart.js
+++ js/chart.js
@@ -133,30 +133,6 @@ $(function () {
         borderWidth: 1,
         fill: true, // 3: no fill
       },
-      {
-        data: [20, 30, 15, 20, 12, 52],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255,99,132,1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-        fill: true, // 3: no fill
-
-        // Changes this dataset to become a line
-        type: "bar",
-      },
     ],
   };
 
@@ -203,14 +179,6 @@ $(function () {
         borderWidth: 1,
         fill: true,
       },
-      {
-        label: "Linkedin",
-        data: [6, 14, 16, 20, 12, 18, 15, 12, 17, 19, 15, 11],
-        borderColor: ["rgba(255, 206, 86, 0.5)"],
-        backgroundColor: ["rgba(255, 206, 86, 0.5)"],
-        borderWidth: 1,
-        fill: true,
-      },
     ],
   };
 
@@ -222,8 +190,11 @@ $(function () {
     },
     elements: {
       point: {
-        radius: 0,
+        radius: 3,
       },
+    },
+    legend: {
+      display: false,
     },
     scales: {
       xAxes: [
